240a0210fdd48d3721d271978b32267ac97a48aa
minouboo <minouboo369@yahoo.com>
2023-09-15 12:43:09 +0700

    Minh Commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Minh</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#222831',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
